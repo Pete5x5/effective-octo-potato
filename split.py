f872722bec5ee7aef3799a06738c4ad9963ff563
Peter <Pete5x5@users.noreply.github.com>
2019-03-15 12:38:24 -0400

    Add models, split

---
+++ split.py
@@ -0,0 +1,19 @@
+import sys      #Enable system functionality
+import PyPDF2   #Enable PDF functionality
+import datetime #Enable date and time functionality
+
+gendate = (datetime.datetime.now()).strftime('%Y%m%d-%H%M%S') #set the date and time the files are generated
+
+file1 = str(sys.argv[1]) #get the file name from the first argument
+
+pdf1 = open(file1, 'rb') #set the open file as pdf1
+
+pdf1Reader = PyPDF2.PdfFileReader(pdf1)
+pdfWriter = PyPDF2.PdfFileWriter()
+
+for pageNum in range(pdf1Reader.numPages):
+    singlepage = pdf1Reader.getPage(pageNum)
+    pdfWriter.addPage(singlepage)
+    pdfOutputFile = open(str(int(pageNum+1)) + '-' + gendate + '.pdf', 'wb')
+    pdfWriter.write(pdfOutputFile)
+    pdfOutputFile.close()
